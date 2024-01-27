58b90060e0e4a59f81e28f01ca4e750526ab5ee5
w1la <w1la920@gmail.com>
2024-01-27 15:33:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[]args) {
+        Scanner scan =  new Scanner(System.in);
+        int[] a = {scan.nextInt()};
+        int sum =0;
+        for (int i = 0;i<43;i++)
+            sum=+a[i];
+
+
+        System.out.println(sum);
+    }}
---
+++ src/section11_decopmosition/PrintArrayElementsMoreThanAvgVersion3.java
@@ -0,0 +1,7 @@
+package section11_decopmosition;
+
+public class PrintArrayElementsMoreThanAvgVersion3 {
+    public static void main(String[] args) {
+
+    }
+}
